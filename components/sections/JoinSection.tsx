2cadfd1fe7508604fe2d52f27a5622bd5fe6cc7c
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-05-30 20:22:14 +0530

    Join Section: animated component elements added

--- components/sections/JoinSection.tsx
+++ components/sections/JoinSection.tsx
@@ -2,6 +2,7 @@ import * as React from "react"
 
 import Link from "next/link"
 
+import AnimatedComponent from "../AnimatedComponent"
 import Button from "../atoms/Button"
 import Container from "../atoms/Container"
 import ArrowRight from "../vectors/ArrowRight"
@@ -10,16 +11,8 @@ const JoinSection: React.FC = () => {
   return (
     <section className="py-20">
       <Container className="grid grid-cols-2 items-center gap-10">
-        <div className="overflow-hidden rounded-3xl">
-          <svg
-            width="572"
-            height="508"
-            viewBox="0 0 572 508"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <rect width="572" height="508" fill="#180020" />
-          </svg>
+        <div className="relative overflow-hidden rounded-3xl">
+          <AnimatedComponent />
         </div>
         <div>
           <div className="ml-[102px] max-w-[470px]">
@@ -31,7 +24,7 @@ const JoinSection: React.FC = () => {
               impact. Unlock membership for a deeper dive into the world of
               artificial intelligence.
             </p>
-            <Link href={"/app/"}>
+            <Link href="/app/">
               <Button variant="dark" isOutline iconRight={<ArrowRight />}>
                 Join now
               </Button>
