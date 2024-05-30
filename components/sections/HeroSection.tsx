2832d360a77c541b7c7001e6b2899939e2d90ac5
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-05-30 19:18:47 +0530

    Hero Section Styled

--- components/sections/HeroSection.tsx
+++ components/sections/HeroSection.tsx
@@ -1,23 +1,53 @@
+"use client"
 import * as React from "react"
+
+import Image from "next/image"
+import Link from "next/link"
 
 import Button from "../atoms/Button"
 import Container from "../atoms/Container"
+import ArrowRight from "../vectors/ArrowRight"
+import HeroImage from "assets/hero.png"
 
 const HeroSection: React.FC = () => {
+  const scrollToFooter = (): void => {
+    const footer = document.querySelector("footer")
+    footer?.scrollIntoView({ behavior: "smooth" })
+  }
+
   return (
-    <header className="py-[88px]">
-      <Container>
+    <header className="relative bg-cover bg-no-repeat py-[88px] text-white">
+      <Image
+        src={HeroImage}
+        alt="Hero Image"
+        placeholder="blur"
+        className="absolute inset-0 z-0 w-full"
+      />
+      <Container className="relative z-10">
         <div className="max-w-[470px]">
-          <h1 className="mb-8 font-heading text-6xl font-bold">
+          <h1 className="mb-8 font-heading text-6xl font-semibold">
             Top insights from AI talks
           </h1>
-          <p className="mb-10">
+          <p className="mb-10 text-xl tracking-tight">
             Get informed about the latest talks in artificial intelligence, all
             from the comfort of your front room
           </p>
           <div className="flex gap-3">
-            <Button>Get the app</Button>
-            <Button variant="secondary">Join a talk</Button>
+            <Link href="/app">
+              <Button variant="primary" iconRight={<ArrowRight />}>
+                Get the app
+              </Button>
+            </Link>
+            <Button
+              variant="light"
+              isOutline
+              iconRight={<ArrowRight />}
+              onClick={() => {
+                scrollToFooter()
+              }}
+            >
+              Join a talk
+            </Button>
           </div>
         </div>
       </Container>
