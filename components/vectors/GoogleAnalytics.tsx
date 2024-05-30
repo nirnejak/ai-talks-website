018f864f85387a27e407086de3962c297942d3b4
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-05-30 21:27:11 +0530

    Animation elements added

---
+++ components/vectors/GoogleAnalytics.tsx
@@ -0,0 +1,70 @@
+import * as React from "react"
+
+const GoogleAnalytics: React.FC = () => {
+  return (
+    <svg
+      width="63"
+      height="62"
+      viewBox="0 0 63 62"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect x="1.5" y="1" width="60" height="60" rx="30" fill="#190E26" />
+      <rect
+        x="1.5"
+        y="1"
+        width="60"
+        height="60"
+        rx="30"
+        fill="white"
+        fill-opacity="0.04"
+      />
+      <rect
+        x="1.5"
+        y="1"
+        width="60"
+        height="60"
+        rx="30"
+        stroke="#190E26"
+        stroke-width="1.5"
+      />
+      <rect
+        x="1.5"
+        y="1"
+        width="60"
+        height="60"
+        rx="30"
+        stroke="url(#paint0_linear_9_1077)"
+        stroke-opacity="0.1"
+        stroke-width="1.5"
+      />
+      <path
+        d="M18.8438 41.125C18.8438 39.261 20.3548 37.75 22.2188 37.75C24.0827 37.75 25.5938 39.261 25.5938 41.125C25.5938 42.989 24.0827 44.5 22.2188 44.5C20.3548 44.5 18.8438 42.989 18.8438 41.125Z"
+        fill="white"
+      />
+      <path
+        d="M28.125 31C28.125 29.136 29.636 27.625 31.5 27.625C33.364 27.625 34.875 29.136 34.875 31V41.125C34.875 42.989 33.364 44.5 31.5 44.5C29.636 44.5 28.125 42.989 28.125 41.125V31Z"
+        fill="white"
+      />
+      <path
+        d="M37.4062 20.875C37.4062 19.011 38.9173 17.5 40.7812 17.5C42.6452 17.5 44.1562 19.011 44.1562 20.875V41.125C44.1562 42.989 42.6452 44.5 40.7812 44.5C38.9173 44.5 37.4062 42.989 37.4062 41.125V20.875Z"
+        fill="white"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_9_1077"
+          x1="31.5"
+          y1="1"
+          x2="31.5"
+          y2="61"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="white" />
+          <stop offset="1" stop-color="white" stop-opacity="0" />
+        </linearGradient>
+      </defs>
+    </svg>
+  )
+}
+
+export default GoogleAnalytics
---
+++ components/vectors/Insights.tsx
@@ -0,0 +1,118 @@
+import * as React from "react"
+
+const Insights: React.FC = () => {
+  return (
+    <svg
+      width="63"
+      height="62"
+      viewBox="0 0 63 62"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect
+        x="1.02002"
+        y="1"
+        width="60.48"
+        height="60"
+        rx="30"
+        fill="#190E26"
+      />
+      <rect
+        x="1.02002"
+        y="1"
+        width="60.48"
+        height="60"
+        rx="30"
+        fill="white"
+        fill-opacity="0.08"
+      />
+      <rect
+        x="1.02002"
+        y="1"
+        width="60.48"
+        height="60"
+        rx="30"
+        stroke="url(#paint0_linear_9_1080)"
+        stroke-opacity="0.1"
+        stroke-width="1.5"
+      />
+      <g clip-path="url(#clip0_9_1080)">
+        <path
+          d="M26.4883 12.9795L22.6205 16.8347L26.4883 20.6899L30.3561 16.8347L26.4883 12.9795Z"
+          fill="white"
+        />
+        <path
+          d="M36.0661 12.9925L32.1983 16.8477L36.0661 20.7028L39.934 16.8477L36.0661 12.9925Z"
+          fill="white"
+        />
+        <path
+          d="M36.073 31.7513L32.2052 35.6064L36.073 39.4616L39.9409 35.6064L36.073 31.7513Z"
+          fill="white"
+        />
+        <path
+          d="M26.8961 41.7439L30.3148 45.1515L26.4537 49L22.5925 45.1515L26.0515 41.7439V39.0178L23.0349 36.0513H20.2999L16.8812 39.4588L13.02 35.6103L16.8812 31.7617L20.2999 35.1693H23.0349L26.4537 31.7617L30.3148 35.6103L26.8961 39.0178V41.7439Z"
+          fill="url(#paint1_linear_9_1080)"
+          fill-opacity="0.4"
+        />
+        <path
+          d="M36.0599 41.2979L32.1921 45.1531L36.0599 49.0083L39.9277 45.1531L36.0599 41.2979Z"
+          fill="white"
+        />
+        <path
+          d="M45.6794 31.7361L41.8115 35.5913L45.6794 39.4465L49.5472 35.5913L45.6794 31.7361Z"
+          fill="white"
+        />
+        <path
+          d="M26.4801 22.1631L22.6123 26.0183L26.4801 29.8735L30.348 26.0183L26.4801 22.1631Z"
+          fill="white"
+        />
+        <path
+          d="M16.8738 22.1783L13.006 26.0334L16.8738 29.8886L20.7416 26.0334L16.8738 22.1783Z"
+          fill="white"
+        />
+        <path
+          d="M36.058 22.1763L32.1901 26.0315L36.058 29.8867L39.9258 26.0315L36.058 22.1763Z"
+          fill="white"
+        />
+        <path
+          d="M45.6642 22.1609L41.7964 26.0161L45.6642 29.8713L49.532 26.0161L45.6642 22.1609Z"
+          fill="white"
+        />
+      </g>
+      <defs>
+        <linearGradient
+          id="paint0_linear_9_1080"
+          x1="31.26"
+          y1="1"
+          x2="31.26"
+          y2="61"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="white" stop-opacity="0.83" />
+          <stop offset="1" stop-color="white" stop-opacity="0.79" />
+        </linearGradient>
+        <linearGradient
+          id="paint1_linear_9_1080"
+          x1="21.6674"
+          y1="31.7617"
+          x2="21.6674"
+          y2="49"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="white" stop-opacity="0.32" />
+          <stop offset="1" stop-color="white" stop-opacity="0.32" />
+        </linearGradient>
+        <clipPath id="clip0_9_1080">
+          <rect
+            width="36.48"
+            height="36"
+            fill="white"
+            transform="translate(13.02 13)"
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  )
+}
+
+export default Insights
