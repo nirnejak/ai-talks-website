4f6aa36aa7be9dc0d71b4fb81bb09a7d5fd371e7
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-05-30 20:50:12 +0530

    talks content added

---
+++ utils/talks.tsx
@@ -0,0 +1,71 @@
+const talks = [
+  {
+    city: "London",
+    title: "AI Revolution: Shaping the Future Together",
+    subtitle:
+      "Join us for an inspiring discussion on the AI Revolution and its collaborative impact on shaping a shared future.",
+    imageUrl: require("assets/card-1.png"),
+  },
+  {
+    city: "London",
+    title: "AI Revolution: Shaping Tomorrow's World",
+    subtitle:
+      "Delve into the transformative power of AI and its profound impact on shaping the future landscape. Explore the intricate interplay between artificial intelligence and various sectors, uncovering how it's redefining industries, innovation, and the very fabric of society.",
+    imageUrl: require("assets/card-2.png"),
+  },
+  {
+    city: "London",
+    title: "AI Transformation: Building a Future of Innovation",
+    subtitle:
+      "Discover how AI is driving innovation and shaping the future. Join us in exploring the transformative power of AI.",
+    imageUrl: require("assets/card-3.png"),
+  },
+  {
+    city: "Singapore",
+    title:
+      "Data Alchemy: Unveiling the Secrets of AI-Powered Predictive Analytics",
+    subtitle:
+      "Discover the magic behind predictive analytics powered by AI, unraveling how data alchemy turns raw information into valuable insights, revolutionizing decision-making across industries.",
+    imageUrl: require("assets/card-4.png"),
+  },
+  {
+    city: "Singapore",
+    title: "Ethical AI: Navigating the Moral Maze of Autonomous Systems",
+    subtitle:
+      "Delve into the ethical dimensions of AI, exploring the complex terrain of autonomy, accountability, and bias to forge a path toward responsible AI deployment and usage.",
+    imageUrl: require("assets/card-5.png"),
+  },
+  {
+    city: "Singapore",
+    title: "AI and Creativity: Redefining the Boundaries of Human Ingenuity",
+    subtitle:
+      "Dive into the intersection of AI and creativity, examining how machine learning algorithms are reshaping artistic expression, innovation processes, and the very nature of human ingenuity.",
+
+    imageUrl: require("assets/card-6.png"),
+  },
+  {
+    city: "Los Angeles",
+    title:
+      "AI in Healthcare: Transforming Patient Care and Treatment Paradigms",
+    subtitle:
+      "Explore the groundbreaking advancements in healthcare driven by AI, from personalized medicine and early disease detection to streamlining administrative tasks, revolutionizing the patient experience.",
+    imageUrl: require("assets/card-7.png"),
+  },
+  {
+    city: "Los Angeles",
+    title: "AI for Sustainability: Harnessing Technology to Save the Planet",
+    subtitle:
+      "Uncover the pivotal role of AI in addressing pressing environmental challenges, from climate change modeling and resource management to sustainable agriculture and biodiversity conservation.",
+    imageUrl: require("assets/card-8.png"),
+  },
+  {
+    city: "Los Angeles",
+    title:
+      "AI and the Future of Work: Embracing Automation in the Age of Industry 4.0",
+    subtitle:
+      "Explore the impact of AI-driven automation on the workforce, navigating the opportunities and challenges presented by Industry 4.0, and reimagining the future of employment and skills development.",
+    imageUrl: require("assets/card-9.png"),
+  },
+]
+
+export default talks
