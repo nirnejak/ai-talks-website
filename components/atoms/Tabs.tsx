8bcbd12beaa89561ef8a3489b3965c1cf9026ffc
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-05-31 14:55:48 +0530

    Tabs simplified and ts issues fixed

--- components/atoms/Tabs.tsx
+++ components/atoms/Tabs.tsx
@@ -1,12 +1,6 @@
-// @ts-nocheck
 import * as React from "react"
 
 import classNames from "@/utils/classNames"
-
-interface TAB_BOUNDING_BOX_TYPE {
-  left: number
-  width: number
-}
 
 interface Props {
   tabsOptions: string[]
@@ -21,47 +15,30 @@ const Tabs: React.FC<Props> = ({
   setActiveTab,
   className,
 }) => {
-  const [tabBoundingBox, setTabBoundingBox] =
-    React.useState<TAB_BOUNDING_BOX_TYPE | null>(null)
-  const [wrapperBoundingBox, setWrapperBoundingBox] =
-    React.useState<TAB_BOUNDING_BOX_TYPE | null>(null)
-  const [highlightedTab, setHighlightedTab] = React.useState<string>(
-    tabsOptions[activeTab]
-  )
-
   const highlightRef = React.useRef<HTMLDivElement | null>(null)
   const wrapperRef = React.useRef<HTMLDivElement | null>(null)
-  const tabRefs = React.useRef<Array<HTMLButtonElement | null>>([])
+
+  const [highlightStyles, setHighlightStyles] =
+    React.useState<React.CSSProperties>({ opacity: 0 })
 
   React.useEffect(() => {
-    if (wrapperRef.current != null && tabRefs.current[activeTab] != null) {
-      setWrapperBoundingBox(wrapperRef.current.getBoundingClientRect())
-      setTabBoundingBox(tabRefs.current[activeTab]!.getBoundingClientRect())
+    if (wrapperRef?.current !== null) {
+      const elements = wrapperRef.current?.getElementsByTagName("button")
+
+      if (elements?.length !== 0) {
+        const activeTabElement = elements[activeTab]
+        const dimensions = activeTabElement.getBoundingClientRect()
+        const wrapperDimensions = wrapperRef.current.getBoundingClientRect()
+
+        setHighlightStyles({
+          opacity: 1,
+          transitionDuration: "150ms",
+          width: `${dimensions.width}px`,
+          transform: `translate(${dimensions.left - wrapperDimensions.left}px)`,
+        })
+      }
     }
   }, [activeTab])
-
-  const repositionHighlight = (
-    e:
-      | React.MouseEvent<HTMLButtonElement, MouseEvent>
-      | React.FocusEvent<HTMLButtonElement>,
-    tab: string,
-    index: number
-  ): void => {
-    setTabBoundingBox((e.target as HTMLButtonElement).getBoundingClientRect())
-    if (wrapperRef.current != null)
-      setWrapperBoundingBox(wrapperRef.current.getBoundingClientRect())
-    setHighlightedTab(tab)
-    setActiveTab(index)
-  }
-
-  const highlightStyles: React.CSSProperties = {}
-
-  if (tabBoundingBox != null && wrapperBoundingBox != null) {
-    highlightStyles.transitionDuration = "150ms"
-    highlightStyles.opacity = `${highlightedTab != null ? 1 : 0}`
-    highlightStyles.width = `${tabBoundingBox.width}px`
-    highlightStyles.transform = `translate(${tabBoundingBox.left - wrapperBoundingBox.left}px)`
-  }
 
   return (
     <nav
@@ -69,24 +46,17 @@ const Tabs: React.FC<Props> = ({
       ref={wrapperRef}
     >
       <div
-        className="absolute left-0 h-full rounded-full border border-purple-200 bg-purple-200"
+        className="absolute left-0 h-full rounded-full border border-purple-200 bg-purple-200 transition-all"
         ref={highlightRef}
-        style={{
-          transition: "0.15s ease",
-          transitionProperty: "width, transform, opacity",
-          ...highlightStyles,
-        }}
+        style={highlightStyles}
       />
       {tabsOptions.map((tab, index) => (
         <button
           key={index}
-          className={
-            "relative inline-block rounded-full border border-transparent px-3 py-1.5 text-sm font-semibold text-slate-900 outline-none hover:border-purple-100 focus:border-purple-100 md:px-5 md:py-2 md:text-base"
-          }
-          onClick={(ev: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-            repositionHighlight(ev, tab, index)
+          className="relative inline-block rounded-full border border-transparent px-3 py-1.5 text-sm font-semibold text-slate-900 outline-none hover:border-purple-100 focus:border-purple-100 md:px-5 md:py-2 md:text-base"
+          onClick={() => {
+            setActiveTab(index)
           }}
-          ref={(el) => (tabRefs.current[index] = el)}
         >
           {tab}
         </button>
